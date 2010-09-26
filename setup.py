1620d4a66170e45328ff7bb232bd370eaeec15af
AZ Huang <aitjcize@gmail.com>
2010-09-26 23:02:21 +0800

    Version 0.1.3: fix serious bug

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ files = ["doc/README",
 
 setup(
 	name = 'pttxp',
-	version = '0.1.2',
+	version = '0.1.3',
 	description = 'Ptt Posting/Scripting Utility',
 	author = 'Wei-Ning Huang (AZ)',
 	author_email = 'aitjcize@gmail.com',
